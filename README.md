2d5dfe7a8bd6677194df8c5d25b136226deba015
astra-the-boop <astra.the.boop@icloud.com>
2025-06-28 21:44:37 +0700

    updated the python verifyer script... i suck at spelling omg. but yeah it works with the new physical ballot voter ID system AND has env support now :D no more hard coding

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # HC Parliament Voter ID Registration System
-### Unique Voter identification system for Hack Club Parliament (Client side)
+### Unique Voter identification system for Hack Club Parliament
 
 #### The Parliament of Hack Club (Hack Club Parliament) and other associated communities and entities are not associated with the Hack Club non-profit organization. This is an unofficial community group and only exists for the purpose of entertainment.
--- verifier.py
+++ verifier.py
@@ -1,6 +1,13 @@
 import base64
+import os
+from dotenv import load_dotenv
+import hmac
+import hashlib
+import re
 
-pass = ""
+load_dotenv()
+
+passw = os.getenv("ENCRYPT_KEY")
 
 def xor_encrypt(data, key):
     key = (key * (len(data) // len(key) + 1))[:len(data)]
@@ -20,12 +27,50 @@ def custom_encrypt(plaintext, password):
     final_result = final_base64_encoded.rstrip("=")
     return final_result
 
+def physicalCode(text, key):
+    digest = hmac.new(key.encode(), text.encode(), hashlib.sha256).digest()
+
+    base36 = hex(int.from_bytes(digest, 'big'))[2:]  # remove '0x'
+    base36 = int(base36, 16)
+    base36_str = base36_to_str(base36).upper()
+
+    alphanum = re.sub(r'[^A-Z0-9]', '', base36_str)
+
+    # Return first 6 chars, padded with 'X' if needed
+    return alphanum[:6].ljust(6, 'X')
+
+def base36_to_str(num):
+    chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    result = ''
+    while num > 0:
+        num, i = divmod(num, 36)
+        result = chars[i] + result
+    return result or '0'
+
+def voterBlock(input_str, key):
+    digest = hmac.new(key.encode(), input_str.encode(), hashlib.sha256).digest()
+    return digest[0] % 3
+
 while True:
-    x = input("Enter voter ID_")
-    y = input("Enter Slack ID_")
-    if custom_encrypt(y, pass) == x:
-        print("Not fraud!! Yayyy!")
+    if input("[d]igital or [p]hysical").lower() == "d":
+        x = input("Enter voter ID_")
+        y = input("Enter Slack ID_")
+        if custom_encrypt(y, passw) == x:
+            print("Not fraud!! Yayyy!")
+        else:
+            print("Fraud :(")
+            print(custom_encrypt(y, passw))
+            print(x)
     else:
-        print("Fraud :(")
-        print(custom_encrypt(y, pass))
-        print(x)
+        x = input("Enter voter ID_")
+        y = input("Enter Slack ID_")
+        z = input("Enter voter block_")
+
+        if physicalCode(custom_encrypt(y, passw), passw) == x and voterBlock(custom_encrypt(y, passw), passw) == z:
+            print("Not fraud!! Yayyy!")
+        else:
+            print("Fraud likely :(")
+            print(physicalCode(custom_encrypt(y, passw), passw))
+            print(voterBlock(custom_encrypt(y, passw), passw))
+            print(x)
+            print(z)
